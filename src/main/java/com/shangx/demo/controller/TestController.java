bb352aa8d933c7d1a1008e22912cf86d16470aad
belter <468092550@qq.com>
2019-03-19 17:17:01 +0800

    SpringBoot示例

--- src/main/java/com/shangx/demo/controller/TestController.java
+++ src/main/java/com/shangx/demo/controller/TestController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+
     @RequestMapping("/hello")
     public  String sayHello(){
         return "HelloWord";
